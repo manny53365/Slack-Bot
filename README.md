634224b752a71eddd440c32132abaaae51ea8ad7
manny53365 <45837705+manny53365@users.noreply.github.com>
2022-12-06 16:24:50 -0500

    refactored the code to work with the genez.io platform

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# slackbot
-Slackbot for customer
+# Slack Bot using Genez.io
+
+## How it works
+
+The slack bot is designed to help a user in the following ways.
+
+1. Automate the process of checking your Etherum node(s).
+2. Send an automated message the the channel id specified in the .env file. The message will inform you of your node's status (i.e good or not responding/out of sync).
+3. Should your node ever fall out of sync there are 3 potential potential fixes offered by the bot.
+
+- The bot only informs the user and takes no action
+- The bot will automatically send a reboot to the AWS instance(s) specified in the .env file.
+- The bot will send a button to the slack chat asking the user for confirmation to reboot the AWS instance(s).
+
+### Setting up your environment
+
+When you download the files to your local system and unzip them in the directory you wish to use, the command install all of the node dependencies is `npm i`.
+Once all of this has completed, you're ready to begin. This bot is using the genez.io platform, to install the dependencies and get this set up you can follow the instructions here [Genezio Github](https://github.com/Genez-io/genezio).
+
+The genezio.yaml file will house your cron frequency (how often the check is done), by default I have set this to 5 minutes. This cron setting is based off of the AWS model, information to configure can be found here [Cron Expressions Documentation](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html).
+
+I have also included a dummy .env file with the proper variable names laid out. **You will need to enter your own value information for the code to work. I recommend you keep the variable names the same**
--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 require('dotenv').config();
 
-// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
 const { WebClient, LogLevel } = require("@slack/web-api");
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cron = require('./cron');
-
-const app = express();
-
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-//Project requirements: 
-// The slack bot should me keep my Ethereum Node up, running and syncing without issues and with minimal cost. 
-// The Slack Bot will notify me when the Ethereum Node is synced or if it is downed. 
-// If the node is having a problem I can ask the Slack Bot to restart the Node.
-
-// WebClient instantiates a client that can call API methods
-// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient(process.env.BOT_TOKEN, {
-    // LogLevel can be imported and used to make debugging simpler
-    logLevel: LogLevel.DEBUG
-  });
-
-// ID of the channel you want to send the message to
-const channelId = process.env.CHANNEL_ID;
+const axios = require('axios');
 
 //challenge code for bot on slack
 
@@ -36,50 +12,68 @@ const channelId = process.env.CHANNEL_ID;
 //   res.send({challenge})
 // });
 
-app.get('/', function(req,res){
-  res.send('This is an API');
-})
+export class checkNodeHealth {
 
-try {
-    // Call the chat.postMessage method using the WebClient
-    const result = client.chat.postMessage({
-      channel: channelId,
-      text: "Bot initialized"
+  checkNodeStatusTest = () => {
+
+    const client = new WebClient(process.env.BOT_TOKEN, {
+      // LogLevel can be imported and used to make debugging simpler
+      logLevel: LogLevel.DEBUG
     });
-  
-    console.log(result);
+
+    // ID of the channel you want to send the message to
+    const channelId = process.env.CHANNEL_ID;
+
+    // block number trackers
+    let ethBlockNum = 0;
+    let bWareBlockNum = 0;
+
+    const data = {
+      jsonrpc:"2.0",
+      method:"eth_blockNumber",
+      params:[],
+      id:1
+    };
+
+    async function ethBlockNumFunc() {
+      try{
+        const response = await axios.post(process.env.ENDPOINT_URL,data)
+        ethBlockNum = BigInt(response.data.result).toString();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    ethBlockNumFunc();
+
+    async function bWareBlockNumFunc() {
+      try{
+        const response = await axios.post(process.env.BWARE_NODE_URL, data)
+        bWareBlockNum =  BigInt(response.data.result).toString();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    bWareBlockNumFunc();
+
+    async function differenceCalculation() {
+      let difference = Math.abs(ethBlockNum - bWareBlockNum);
+
+      let text;
+
+      difference <= 3 ? text=`Your Etherum node is healthy.` : text=`Your Etherum node is out of sync.`
+
+      try {
+        await client.chat.postMessage({
+          channel: channelId,
+          text: `${text}`
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    differenceCalculation();
   }
-  catch (error) {
-    console.error(error);
-  };
-
-// event for app mention to bot in the 'general' channel
-app.post('/', function(req,res) {
-    let payload = req.body;
-    res.sendStatus(200);
-    
-    if (payload.event.type === 'app_mention') {
-      if (payload.event.text.includes(' status')){
-        client.chat.postMessage(
-          {channel: channelId,
-          text: "Status request received"
-          }
-        
-        )
-      }
-    };
-  
-    if (payload.event.type === 'app_mention') {
-      if (payload.event.text.includes(' help')){
-        client.chat.postMessage(
-          {channel: channelId,
-          text: "Use \'@status bot status\' to check the node status"
-          }
-        )
-      }
-    };
-  })
-
-app.listen(3000, function(){
-  console.log('Started listening on port 3000');
-})
+}
