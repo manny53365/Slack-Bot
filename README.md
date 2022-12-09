7b7065a31e42f17ec96f3cbf5b4cfed8b22f8970
manny53365 <45837705+manny53365@users.noreply.github.com>
2022-12-09 14:57:51 -0500

    Added logic for repairing out of sync nodes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## How it works
 
-The slack bot is designed to help a user in the following ways.
+The slack bot is designed to help a user in the following ways
 
 1. Automate the process of checking your Etherum node(s).
 2. Send an automated message the the channel id specified in the .env file. The message will inform you of your node's status (i.e good or not responding/out of sync).
@@ -12,11 +12,27 @@ The slack bot is designed to help a user in the following ways.
 - The bot will automatically send a reboot to the AWS instance(s) specified in the .env file.
 - The bot will send a button to the slack chat asking the user for confirmation to reboot the AWS instance(s).
 
-### Setting up your environment
+---
 
-When you download the files to your local system and unzip them in the directory you wish to use, the command install all of the node dependencies is `npm i`.
+## Setting up your environment
+
+When you download the files to your local system and unzip them in the directory you wish to use. Open the command prompt in your working directory where you unzipped your files and install all of the node dependencies by running the command `npm i`.
 Once all of this has completed, you're ready to begin. This bot is using the genez.io platform, to install the dependencies and get this set up you can follow the instructions here [Genezio Github](https://github.com/Genez-io/genezio).
 
 The genezio.yaml file will house your cron frequency (how often the check is done), by default I have set this to 5 minutes. This cron setting is based off of the AWS model, information to configure can be found here [Cron Expressions Documentation](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html).
 
-I have also included a dummy .env file with the proper variable names laid out. **You will need to enter your own value information for the code to work. I recommend you keep the variable names the same**
+I have also included a dummy .env file with the proper variable names laid out. **You will need to enter your own value information for the code to work. I recommend you keep the variable names the same.**
+
+---
+
+### Configuration
+
+The `cron` configuration is located in your genezio.yaml file. The second config is the .env file. An important parameter to keep in mind is the parameter to handle the repair logic. The valid values are one of the following `none || auto || manual`.
+
+The `none` configuration value will just send a message to the slack chat to inform the user the node is out of sync.
+
+The `auto` value will automatically send a reboot to the AWS EC2 instance.
+
+The `manual` option will send an interactive message to the selected slack channel containing a selection 'Approve' or 'Reject' button for the user. Upon selecting 'Approve' the AWS reboot will be triggered in the background by the bot.
+
+Webhooks are a very vital part of this bot for `manual` configurations. In slack, you will need to set up and configure your incoming webhooks. [Slack Webhooks Documentation](https://api.slack.com/messaging/webhooks)
--- app.js
+++ app.js
@@ -2,19 +2,13 @@ require('dotenv').config();
 
 const { WebClient, LogLevel } = require("@slack/web-api");
 const axios = require('axios');
-
-//challenge code for bot on slack
-
-// app.post('/', function(req,res){
-
-//   const {challenge} = req.body;
-
-//   res.send({challenge})
-// });
+const AWS = require("aws-sdk");
 
 export class checkNodeHealth {
 
-  checkNodeStatusTest = () => {
+  async checkNodeStatusTest(){
+
+    const self = this;
 
     const client = new WebClient(process.env.BOT_TOKEN, {
       // LogLevel can be imported and used to make debugging simpler
@@ -39,41 +33,164 @@ export class checkNodeHealth {
       try{
         const response = await axios.post(process.env.ENDPOINT_URL,data)
         ethBlockNum = BigInt(response.data.result).toString();
+        return ethBlockNum = 0
       } catch (err) {
         console.log(err);
       }
     };
-
-    ethBlockNumFunc();
 
     async function bWareBlockNumFunc() {
       try{
-        const response = await axios.post(process.env.BWARE_NODE_URL, data)
+        const response = await axios.post(process.env.BWARE_NODE_URL,data)
         bWareBlockNum =  BigInt(response.data.result).toString();
+        return bWareBlockNum
       } catch (err) {
         console.log(err);
       }
     };
-
-    bWareBlockNumFunc();
 
     async function differenceCalculation() {
       let difference = Math.abs(ethBlockNum - bWareBlockNum);
 
-      let text;
+      difference <= 3 ? nodeIsHealthy() :
 
-      difference <= 3 ? text=`Your Etherum node is healthy.` : text=`Your Etherum node is out of sync.`
+      difference > 3  && process.env.REPAIR_OPTION === 'none' ? informUserOutOfSync() : difference > 3  && process.env.REPAIR_OPTION === 'auto' ? rebootEC2Instance() : difference > 3  && process.env.REPAIR_OPTION === 'manual' ? self.outOfSyncNodeHandlerReboot() : configIsInvalid();
+    };
 
+    async function nodeIsHealthy(){
       try {
         await client.chat.postMessage({
           channel: channelId,
-          text: `${text}`
+          text: `Your Etherum node is healthy.`
         });
       } catch (err) {
-        console.log(err);
-      }
+          console.log(err);
+        }
     };
 
-    differenceCalculation();
+    async function informUserOutOfSync(){
+      try {
+        await client.chat.postMessage({
+          channel: channelId,
+          text: `Your Etherum node is out of sync.`
+        });
+      } catch (err) {
+          console.log(err);
+        }
+    };
+
+    async function rebootEC2Instance() {
+
+      const ec2 = new AWS.EC2({accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+      });
+    
+      console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.AWS_REGION);
+    
+      const params = {
+        InstanceIds: [process.env.INSTANCE_ID]
+      };
+    
+      try {
+        const data = ec2.rebootInstances(params)
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+
+      client.chat.postMessage({
+        channel: channelId,
+        text: `Your Etherum node is out of sync, rebooting...`
+      });
+
+    };
+
+    async function configIsInvalid(){
+      try {
+        await client.chat.postMessage({
+          channel: channelId,
+          text: `The value you entered '${process.env.REPAIR_OPTION}' is not valid. The valid options available are 'none', 'auto' or 'manual', please update your .env file.`
+        });
+      } catch (err) {
+          console.log(err);
+        }
+    }
+
+    async function performAllActions(){
+      const ethBlockNumFuncResult = await ethBlockNumFunc();
+      const bwareBlockNumFuncResult = await bWareBlockNumFunc();
+      const doCalculation = await differenceCalculation();
+    return ethBlockNumFuncResult, bwareBlockNumFuncResult, doCalculation
+    };
+
+    await performAllActions();
   }
+
+  async outOfSyncNodeHandlerReboot() {
+
+    const message = {
+      "blocks": [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Your node is out of sync, would you like to reboot?",
+            "emoji": true
+          }
+        },
+        {
+          "type": "actions",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Approve"
+              },
+              "style": "primary",
+              "value": "Approve"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Reject"
+              },
+              "style": "danger",
+              "value": "Reject"
+            }
+          ]
+        }
+      ]
+    }
+
+    const channelId = process.env.CHANNEL_ID;
+
+    try {
+      // Send the initial message with the button elements:
+      const response = await axios.post(process.env.REBOOT_BUTTON, message);
+    
+      console.log('Response:', response);
+      console.log('Response data:', response.data);
+    
+      // Extract the value of the button clicked from the response data:
+      const data = JSON.parse(response.data);
+      const selectedValue = data.blocks[1].elements[0].value;
+    
+      // Create a new object for the response message without the button elements:
+      const resData = {
+        channel: channelId,
+        text: `You selected: ${selectedValue}`
+      };
+    
+      // Send the response message to the Slack channel:
+      await axios.post(process.env.REBOOT_BUTTON, resData);
+    } catch (err) {
+      console.log(err);
+    }
+  }  
+
 }
